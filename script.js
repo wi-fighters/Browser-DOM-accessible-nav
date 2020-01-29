991219a1278e2e2dcedaaa39c733dd8505a19c34
Eva Larumbe <evalarumbe@gmail.com>
2020-01-29 10:07:19 +0100

    Add pseudocode

--- script.js
+++ script.js
@@ -5,8 +5,25 @@
    
     // 2. Write the callback
     const openMenu = function() {
+        // grab the ul
         const mainMenu = document.querySelector('#main-menu');
-        mainMenu.classList.toggle('expanded');
+
+        
+        if () { // ul is collapsed
+            //  expand it:
+
+            // change it visually
+            // change the aria-expanded attribute
+        } else {
+            // collapse it:
+
+            // change it visually
+            // change the aria-expanded attribute
+        }
+
+
+        // update its aria-expanded attribute
+        mainMenu.setAttribute('aria-expanded', 'true');
     };
 
     // 3. Attach the event listener
