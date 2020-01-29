cd3e89bc6d7beb03f1e51e121369b312518bb3f9
Eva Larumbe <evalarumbe@gmail.com>
2020-01-29 10:51:03 +0100

    WIP button is working, code could be cleaner

--- script.js
+++ script.js
@@ -5,28 +5,30 @@
    
     // 2. Write the callback
     const openMenu = function() {
-        // grab the ul
-        const mainMenu = document.querySelector('#main-menu');
 
+        // if ul is expanded
+        console.log(this.getAttribute('aria-expanded'));
+        console.log(typeof this.getAttribute('aria-expanded'));
         
-        if () { // ul is collapsed
+        // TODO: Maybe this condition could be easier to read
+        // we're testing three possible states of aria-expanded
+        if (this.getAttribute('aria-expanded') && this.getAttribute('aria-expanded') !== 'false') {
+            console.log("The program thinks the UL is expanded!");
             //  expand it:
 
-            // change it visually
-            // change the aria-expanded attribute
+            // change it visually by changing the aria-expanded attribute
+            this.setAttribute('aria-expanded', 'false');
         } else {
+            console.log("The program thinks the UL is collapsed!");
             // collapse it:
 
-            // change it visually
-            // change the aria-expanded attribute
+            // change it visually by changing the aria-expanded attribute
+            this.setAttribute('aria-expanded', 'true');
         }
-
-
-        // update its aria-expanded attribute
-        mainMenu.setAttribute('aria-expanded', 'true');
     };
 
     // 3. Attach the event listener
     openButton.addEventListener('click', openMenu, false);
+
 
 })();
